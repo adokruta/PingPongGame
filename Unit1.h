1249077a1da2896211e83d0a2895644466b3d2af
adokruta <adokruta@gmail.com>
2021-04-28 12:54:12 +0200

    Added Background

--- Unit1.h
+++ Unit1.h
@@ -7,10 +7,12 @@
 #include <Controls.hpp>
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
+#include <ExtCtrls.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
+        TShape *Background;
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
@@ -19,4 +21,3 @@ public:		// User declarations
 extern PACKAGE TForm1 *Form1;
 //---------------------------------------------------------------------------
 #endif
- 
